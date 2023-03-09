5400d35a8f3f31e4adbac4af833492d988c44ca2
kallyas <kallyasmedia@gmail.com>
2023-03-09 15:33:33 +0300

    ch: add new changes

--- app.js
+++ app.js
@@ -3,39 +3,63 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+mongoose
+  .connect("mongodb://localhost:27017/wikiDB")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("ERROR:", err.message);
+  });
 
 const articleSchema = {
-    title: String,
-    content: String
-}
-
+  title: String,
+  content: String,
+};
 
 const Article = mongoose.model("Article", articleSchema);
 
-
-app.get("/articles", function(req, res) {
-
-
-
-    Article.find(function(err, foundArticles) {
-        console.log(foundArticles);
-
-    })
-
+app.get("/articles", async (req, res) => {
+  try {
+    const articles = await Article.find({});
+    res.send(articles);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.post("/articles", async (req, res) => {
+  const article = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  try {
+    const savedArticle = await article.save();
+    res.send(savedArticle);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.delete("/articles", async (req, res) => {
+  await Article.deleteMany(function (err) {
+    if (!err) {
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
