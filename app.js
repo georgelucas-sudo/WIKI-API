10e3b2d6bec8d242ed3d9e6212ef2ee28089859c
Yiga George Lucas <georgedalucas111@gmail.com>
2023-03-17 13:17:49 +0300

    used the .route for code refactoring

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose
-<<<<<<< HEAD
     .connect("mongodb://localhost:27017/wikiDB")
     .then(() => {
         console.log("Connected to DB");
@@ -25,56 +24,44 @@ mongoose
 const articleSchema = {
     title: String,
     content: String
-=======
-  .connect("mongodb://localhost:27017/wikiDB")
-  .then(() => {
-    console.log("Connected to DB");
-  })
-  .catch((err) => {
-    console.log("ERROR:", err.message);
-  });
-
-const articleSchema = {
-  title: String,
-  content: String,
->>>>>>> 5400d35a8f3f31e4adbac4af833492d988c44ca2
 };
 
 const Article = mongoose.model("Article", articleSchema);
 
-<<<<<<< HEAD
-app.get("/articles", async(req, res) => {
-    try {
-        const articles = await Article.find({});
-        res.send(articles);
-    } catch (err) {
-        res.send(err);
-    }
-});
+app.route("/articles") //refactored the code by using chain method
 
-app.post("/articles", async(req, res) => {
-    const article = new Article({
-        title: req.body.title,
-        content: req.body.content,
-    });
-    try {
-        const savedArticle = await article.save();
-        res.send(savedArticle);
 
-    } catch (err) {
-        res.send(err);
-    }
-});
-
-app.delete("/articles", async(req, res) => {
-    await Article.deleteMany(function(err) {
-        if (!err) {
-            res.send("Successfully deleted all articles.");
-        } else {
+.get(async(req, res) => {
+        try {
+            const articles = await Article.find({});
+            res.send(articles);
+        } catch (err) {
             res.send(err);
         }
-    });
-});
+    })
+    .post(async(req, res) => {
+        const article = new Article({
+            title: req.body.title,
+            content: req.body.content,
+        });
+        try {
+            const savedArticle = await article.save();
+            res.send(savedArticle);
+
+        } catch (err) {
+            res.send(err);
+        }
+    })
+    .delete(async(req, res) => {
+        await Article.deleteMany(function(err) {
+            if (!err) {
+                res.send("Successfully deleted all articles.");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
 
 // Article.find({}).then((articles) => { // find all articles in the database and return them as an array
 
@@ -83,48 +70,29 @@ app.delete("/articles", async(req, res) => {
 //     console.log(err);
 
 app.post("/articles", function(req, res) {
-    console.log(req.body.title);
-    console.log(req.body.content);
+    console.log();
+    console.log();
 
-})
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function(err) {
+        if (!err) {
+            res.send("successfully added new article")
+
+        } else {
+            res.send(err)
+        }
+
+
+
+    });
+});
+
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
-=======
-app.get("/articles", async (req, res) => {
-  try {
-    const articles = await Article.find({});
-    res.send(articles);
-  } catch (err) {
-    res.send(err);
-  }
-});
-
-app.post("/articles", async (req, res) => {
-  const article = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  try {
-    const savedArticle = await article.save();
-    res.send(savedArticle);
-  } catch (err) {
-    res.send(err);
-  }
-});
-
-app.delete("/articles", async (req, res) => {
-  await Article.deleteMany(function (err) {
-    if (!err) {
-      res.send("Successfully deleted all articles.");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
->>>>>>> 5400d35a8f3f31e4adbac4af833492d988c44ca2
